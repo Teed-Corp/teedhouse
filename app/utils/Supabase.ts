7624177a14dd957bb487940518939a9ca3652f32
Lempereur Corentin <73390812+aredli16@users.noreply.github.com>
2024-03-26 15:59:31 +0100

    GH-13: Créer les schémas Prisma BDD

--- app/utils/Supabase.ts
+++ app/utils/Supabase.ts
@@ -2,9 +2,9 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://txcvsebaiugmykgejloe.supabase.co";
+const supabaseUrl = "https://skqxzdzkhlqixrlbtkqq.supabase.co";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4Y3ZzZWJhaXVnbXlrZ2VqbG9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNDc3NzYsImV4cCI6MjAyNTcyMzc3Nn0.mxwJZ3ucSeNhSeRBzQNEavr9oqUEaRdXxxZ9GV8CnJU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrcXh6ZHpraGxxaXhybGJ0a3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNTM3MTQsImV4cCI6MjAyNTcyOTcxNH0.5vI0t8JuwmkpxAe1hzvJj0SiY4uAbGKyImkO0ownUfM";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {

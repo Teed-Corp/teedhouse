c8742992dfa15bbbb9d739d7f52f8399898afce3
Shinotobira <62753305+Shinotobira@users.noreply.github.com>
2024-04-08 10:18:09 +0200

    FEAT: creation de la page rejoindre un groupe

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import ChooseGroupPage from "@app/screens/chooseGroup/ChooseGroupPage";
+import CreateGroupPage from "@app/screens/group/CreateGroupPage";
+import JoinGroupPage from "@app/screens/group/JoinGroupPage";
 import LoginPage from "@app/screens/login/LoginPage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import CreateGroupPage from "@app/screens/group/CreateGroupPage";
 
 export type RootStackParamList = {
   LoginPage: undefined;
   ChooseGroupPage: undefined;
   CreateGroupPage: undefined;
+  JoinGroupPage: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -24,6 +26,7 @@ export default function App() {
           <Stack.Screen name="LoginPage" component={LoginPage} />
           <Stack.Screen name="ChooseGroupPage" component={ChooseGroupPage} />
           <Stack.Screen name="CreateGroupPage" component={CreateGroupPage} />
+          <Stack.Screen name="JoinGroupPage" component={JoinGroupPage} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>

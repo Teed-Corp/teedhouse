9dcd88b9a24c8a5b2fece5a180145bad4a4542be
Aventel <matblairon@gmail.com>
2024-04-19 09:27:52 +0200

    Fix (Create Group): Ajout KeyboardAvoiding

--- app/screens/family/CreateFamilyPage.tsx
+++ app/screens/family/CreateFamilyPage.tsx
@@ -1,15 +1,21 @@
 import ErrorText from "@app/components/Content/ErrorText";
 import HeaderIcon from "@app/components/Content/HeaderIcon";
 import HeaderTitle from "@app/components/Content/HeaderTitle";
+import CustomLoader from "@app/components/CustomLoader";
 import Divider from "@app/components/Divider";
 import AppButton from "@app/components/Inputs/AppButton";
 import CustomDropdown from "@app/components/Inputs/CustomDropdown";
 import CustomTextField from "@app/components/Inputs/CustomTextField";
 import useFamily from "@app/hooks/Family";
 import React, { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomLoader from "@app/components/CustomLoader";
 
 const CreateFamilyPage = () => {
   const { createFamily } = useFamily();
@@ -36,44 +42,46 @@ const CreateFamilyPage = () => {
   };
 
   const renderItem = () => (
-    <View style={styles.content}>
-      <HeaderIcon icon="users" />
-      <Divider height={24} />
-      <HeaderTitle value={"Créer\n une famille"} />
-      <Divider height={20} />
-      <Text style={styles.text}>
-        {
-          "Pour créer votre famille,\nVeuillez remplir les informations ci-dessous"
-        }
-      </Text>
-      <Divider height={24} />
-      <CustomTextField
-        value={familyName}
-        onChangeEvent={setFamilyName}
-        placeHolderValue="Nom de la famille"
-        displayTopPlaceHolder
-      />
-      {(familyName === null || familyName === "") && displayError && (
-        <ErrorText error="Un nom est requis" />
-      )}
-      <Divider height={24} />
-      <CustomDropdown
-        data={data}
-        onSelect={setHomeType}
-        placeHolder="Type de logement"
-        displayTopPlaceHolder
-        value={homeType}
-      />
-      {homeType === null && displayError && (
-        <ErrorText error="Un type de logement est requis" />
-      )}
-      <Divider height={20} />
-      {isLoading ? (
-        <CustomLoader />
-      ) : (
-        <AppButton title="Continuer" onPressEvent={onConfirm} />
-      )}
-    </View>
+    <KeyboardAvoidingView behavior="height">
+      <View style={styles.content}>
+        <HeaderIcon icon="users" />
+        <Divider height={24} />
+        <HeaderTitle value={"Créer\n une famille"} />
+        <Divider height={20} />
+        <Text style={styles.text}>
+          {
+            "Pour créer votre famille,\nVeuillez remplir les informations ci-dessous"
+          }
+        </Text>
+        <Divider height={24} />
+        <CustomTextField
+          value={familyName}
+          onChangeEvent={setFamilyName}
+          placeHolderValue="Nom de la famille"
+          displayTopPlaceHolder
+        />
+        {(familyName === null || familyName === "") && displayError && (
+          <ErrorText error="Un nom est requis" />
+        )}
+        <Divider height={24} />
+        <CustomDropdown
+          data={data}
+          onSelect={setHomeType}
+          placeHolder="Type de logement"
+          displayTopPlaceHolder
+          value={homeType}
+        />
+        {homeType === null && displayError && (
+          <ErrorText error="Un type de logement est requis" />
+        )}
+        <Divider height={20} />
+        {isLoading ? (
+          <CustomLoader />
+        ) : (
+          <AppButton title="Continuer" onPressEvent={onConfirm} />
+        )}
+      </View>
+    </KeyboardAvoidingView>
   );
 
   return (
